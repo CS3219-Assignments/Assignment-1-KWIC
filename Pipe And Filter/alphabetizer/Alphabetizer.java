3ec29573e0a6823c3e60298c0e3ef14f18e1a932
wireless90 <wireless-@hotmail.com>
2016-08-24 18:32:39 +0800

    Changed Alphabetizer to sort only ascending while ignoring case of strings

--- Pipe And Filter/alphabetizer/Alphabetizer.java
+++ Pipe And Filter/alphabetizer/Alphabetizer.java
@@ -1,36 +1,16 @@
 package alphabetizer;
 
-import java.util.*;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 import pipeAndFilter.*;
 
-public class Alphabetizer extends Filter<Iterable<String>, Iterable<String>> implements ISortable<String>{
+public class Alphabetizer extends Filter<Iterable<String>, Iterable<String>> {
 	
 	protected ConcurrentSkipListSet<String> sortedList;
 
 	
 	public Alphabetizer() {
-		sortedList = new ConcurrentSkipListSet<String>();
+		sortedList = new ConcurrentSkipListSet<String>(new SortAscendingIgnoreCase());
 	}
-	
-	@Override
-	public Iterable<String> sortAscending(Iterable<String> collection) {
-		List<String> toBeSorted = new ArrayList<String>();
-		collection.forEach(toBeSorted::add);
-		Collections.sort(toBeSorted, new SortIgnoreCase());
-		
-		return toBeSorted;
-	}
-
-	@Override
-	public Iterable<String> sortDescending(Iterable<String> collection) {
-		List<String> toBeSorted = new ArrayList<String>();
-		collection.forEach(toBeSorted::add);
-		Collections.sort(toBeSorted, Collections.reverseOrder());
-		
-		return toBeSorted;
-	}
-
 	
 }
--- Pipe And Filter/alphabetizer/ISortable.java
+++
@@ -1,6 +0,0 @@
-package alphabetizer;
-
-public interface ISortable<T> {
-	Iterable<T> sortAscending(Iterable<T> collection);
-	Iterable<T> sortDescending(Iterable<T> collection);
-}

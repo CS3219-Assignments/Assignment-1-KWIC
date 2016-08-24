48fcbe84b1f2364eed7404916037a90d89daef65
wireless90 <wireless-@hotmail.com>
2016-08-24 17:55:57 +0800

    Removed redundant imports

--- Pipe And Filter/Program.java
+++ Pipe And Filter/Program.java
@@ -1,17 +1,9 @@
-import output.AsyncOutput;
-import input.AsyncRepository;
-import pipeAndFilter.IAsyncFilter;
-import shifter.AsyncCircularShift;
-import shifter.RightShift;
-import alphabetizer.AsyncAlphabetizer;
-
-
 public class Program {
 
 	public static void main(String[] args){
 		String filePath = "C:\\Users\\User\\Desktop\\input2.txt";
 		
-		new KwicPipeLine(filePath).start();;
+		new KwicPipeLine(filePath).start();
 		
 		
 	}

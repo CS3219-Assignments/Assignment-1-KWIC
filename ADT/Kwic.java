c14a286753a9df6959ad393ec69f7a152ea06782
wireless90 <wireless-@hotmail.com>
2016-08-29 21:15:28 +0800

    forget to do .run()

--- ADT/Kwic.java
+++ ADT/Kwic.java
@@ -6,7 +6,7 @@ public class Kwic{
 
 	public static void main(String args[]){
 		
-		new Kwic(new Input(), new CircularShift(), new AlphaShift(), new Output());
+		new Kwic(new Input(), new CircularShift(), new AlphaShift(), new Output()).run();
 	}
 
 	private IInput in;

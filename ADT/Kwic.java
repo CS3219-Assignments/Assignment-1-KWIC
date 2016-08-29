f1f325975a34d7d6c5baa0a9dab4e95fc3bb359b
tangwr <firebolt2000@hotmail.com>
2016-08-29 21:34:59 +0800

    ADT interface

--- ADT/Kwic.java
+++ ADT/Kwic.java
@@ -3,16 +3,10 @@ import java.io.*;
 
 public class Kwic{
 
-
-	public static void main(String args[]){
-		
-		new Kwic(new Input(), new CircularShift(), new AlphaShift(), new Output());
-	}
-
-	private IInput in;
-	private ICircularShift cs;
-	private IAlphaShift as;
-	private IOutput out;
+	private static IInput in;
+	private static ICircularShift cs;
+	private static IAlphaShift as;
+	private static IOutput out;
 	
 	public Kwic(IInput in, ICircularShift cs, IAlphaShift as, IOutput out){
 		this.in = in;
@@ -20,8 +14,14 @@ public class Kwic{
 		this.as = as;
 		this.out = out;
 	}
-	
-	public void run(){
+
+	public static void main(String args[]){
+		
+		new Kwic(new Input(), new CircularShift(), new AlphaShift(), new Output());
+		run();
+	}
+
+	private static void run(){
 		ArrayList<String> inputList = in.getInputList();
 		ArrayList<String> ignoreList = in.getIgnoreList();
 		
